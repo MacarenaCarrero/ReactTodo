d8772ddec56823e059e12912f302b0d304a8fe9e
MacarenaCarrero <macarena.c.rios@gmail.com>
2025-05-12 21:06:41 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
-import Button from './components/button/Button';
 
 const App = () => {
   return (
     <>
-      <h1>React</h1>
-      <Button />
+    
     </>
-  ); //resultado
+  ); 
 };
 
 export default App;
-//esto es una funcion jsx
